8319ae39f0975151a589defad4de272b3354e90b
Praveenkumaarreddy <praveenkumarreddy0707@gmail.com>
2026-08-01 10:10:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Allow your Vercel frontend to communicate with this Render backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
